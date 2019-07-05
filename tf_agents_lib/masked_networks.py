81fbfb25aa227936947588c4971d7aa47dbc3dde
Sascha Lange <sascha.lange@campus.tu-berlin.de>
2019-07-05 14:08:56 +0200

    added masked ddpg actor and critic and trainscript for t3d

--- tf_agents_lib/masked_networks.py
+++ tf_agents_lib/masked_networks.py
@@ -7,6 +7,9 @@ from tf_agents.networks.actor_distribution_network import ActorDistributionNetwo
 from tf_agents.networks.actor_distribution_rnn_network import ActorDistributionRnnNetwork
 from tf_agents.networks.value_network import ValueNetwork
 from tf_agents.networks.value_rnn_network import ValueRnnNetwork
+from tf_agents.agents.ddpg import critic_rnn_network
+from tf_agents.agents.ddpg import actor_rnn_network
+
 import numpy as np
 
 
@@ -133,6 +136,82 @@ class MaskedValueRnnNetwork(ValueRnnNetwork):
 
     def call(self, observation, step_type=None, network_state=()):
         return super().call(observation['state'], step_type, network_state)
+
+    def __call__(self, inputs, *args, **kwargs):
+        return super(Network, self).__call__(inputs, *args, **kwargs)
+
+
+class MaskedCriticRnnNetwork(critic_rnn_network.CriticRnnNetwork):
+    """A critic network which uses only observation['state'] as observation, to use with ddpg agents.
+
+    For actor-critic methods, the critic network gets the same input
+    as the actor network however only the actor network actually
+    needs the mask, so in the value network we have to throw it away explicitly
+    """
+
+    def __init__(self,
+                 input_tensor_spec,
+                 observation_fc_layer_params,
+                 action_fc_layer_params,
+                 joint_fc_layer_params,
+                 lstm_size,
+                 output_fc_layer_params=None
+                 ):
+        super().__init__(input_tensor_spec['state'],
+                         observation_fc_layer_params=observation_fc_layer_params,
+                         action_fc_layer_params=action_fc_layer_params,
+                         joint_fc_layer_params=joint_fc_layer_params,
+                         lstm_size=lstm_size,
+                         output_fc_layer_params=output_fc_layer_params)
+
+    def call(self, observation, step_type=None, network_state=()):
+        return super().call(observation['state'], step_type, network_state)
+
+    def __call__(self, inputs, *args, **kwargs):
+        return super(Network, self).__call__(inputs, *args, **kwargs)
+
+
+class MaskedActorRnnNetwork(actor_rnn_network.ActorRnnNetwork):
+
+    def __init__(self,
+                 input_tensor_spec,
+                 output_tensor_spec,
+                 conv_layer_params=None,
+                 input_fc_layer_params=(200, 100),
+                 lstm_size=(40,),
+                 output_fc_layer_params=(200, 100),
+                 activation_fn=tf.keras.activations.relu,
+                 name='ActorRnnNetwork'):
+        super(MaskedActorRnnNetwork, self).__init__(
+            input_tensor_spec=input_tensor_spec['state'],
+            output_tensor_spec=output_tensor_spec,
+            conv_layer_params=conv_layer_params,
+            input_fc_layer_params=input_fc_layer_params,
+            output_fc_layer_params=output_fc_layer_params,
+            lstm_size=lstm_size,
+            activation_fn=activation_fn,
+            name=name)
+
+    def call(self, observation, step_type, network_state):
+        states = observation['state']
+        mask = observation['mask']
+
+        # print('MASKS', masks)
+        # print('STATES', states)
+
+        actions, new_network_states = super().call(
+            states, step_type, network_state)
+
+        # for some reason, when we get a batch there is an extra axis of dimension 1
+        # we therefore also have to insert this axis into our mask
+        if len(actions.shape) == 4:
+            mask = tf.expand_dims(mask, 2)
+
+        masked_actions = tf.add(actions, mask)
+
+        # the dtype doesn't refer to the logits
+        # but the action that is then created from the distribution
+        return masked_actions, new_network_states
 
     def __call__(self, inputs, *args, **kwargs):
         return super(Network, self).__call__(inputs, *args, **kwargs)
